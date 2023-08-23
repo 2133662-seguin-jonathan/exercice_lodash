40edba75441557ee45bbad3f41aeee2547768397
2133662-seguin-jonathan <2133662@etudiant.cegepvicto.ca>
2023-08-23 18:56:25 -0400

    Final de l'exercice

--- exercice_lodash.ts
+++ exercice_lodash.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { deburr } from 'lodash';
 
 type Utilisateur = {
   nom: string;
@@ -16,31 +16,31 @@ let utilisateurs: Utilisateur[] = [
 ];
 
 // Avec Lodash, créer un tableau qui ne prend qu'une tranche des utilisateurs à partir du début jusqu'au premier utilisateur inactif
-const premiersActifs = 'À faire';
+const premiersActifs = _.takeWhile(utilisateurs, function(o) { return o.actif; });
 console.log('Liste des premiers utilisateurs actifs :');
 console.log(premiersActifs);
 
 // Avec Lodash, créer un tableau qui contient seulement les utilisateurs de 40 ans et plus
-const utilisateurs40plus = 'À faire';
+const utilisateurs40plus = _.filter(utilisateurs, function(o) { return o.age >= 40; });
 console.log('Liste des utilisateurs de 40 ans et plus :');
 console.log(utilisateurs40plus);
 
 // Avec Lodash, regrouper les utilisateurs par leur age
-const utilisateursParAge = 'À faire';
+const utilisateursParAge = _.groupBy(utilisateurs, "age");
 console.log('Regrouper les utilisateurs par leur âge :');
 console.log(utilisateursParAge);
 
 // Avec Lodash, extrait l'utilisateur le plus vieux
-const utilisateursPlusVieux = 'À faire';
+const utilisateursPlusVieux = _.maxBy(utilisateurs, function(o) { return o.age; });
 console.log('Le plus vieux :');
 console.log(utilisateursPlusVieux);
 
 // Avec Lodash, déterminer l'age moyen
-const ageMoyen = 'À faire';
+const ageMoyen = _.meanBy(utilisateurs, function(o) { return o.age; });
 console.log('Age moyen :');
 console.log(ageMoyen);
 
 // Avec Lodash, remplacer les caractères avec accents et cédilles par leur équivalent sans accent. Ex : é -> e, ç => c
 const phraseAvecAccents = "Ça été un mois d'août très tempéré";
-const phraseSansAccents = 'À faire';
+const phraseSansAccents = _.deburr(phraseAvecAccents);
 console.log(phraseSansAccents);
